feb125fd0b9bec8567b4e9559b0f119ee78bfe9d
Ricky Garcia <rickyg08@msn.com>
2020-09-24 15:20:18 -0600

    Refactoring for loop.

--- src/edu/cnm/deepdive/codebreaker/model/Code.java
+++ src/edu/cnm/deepdive/codebreaker/model/Code.java
@@ -1,6 +1,13 @@
 package edu.cnm.deepdive.codebreaker.model;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
 public class Code {
 
@@ -30,17 +37,49 @@ public class Code {
       this.text = text;
       int correct = 0;
       int close = 0;
-      for (int i = 0; i < secret.length; i++) {
-        char current = secret[i];
-        int postion = text.indexOf(current);
-        if (i == postion) {
+
+      Map<Character, Set<Integer>> letterMap = getLetterMap(text);
+
+      char[] work = Arrays.copyOf(secret, secret.length);
+      for (int i = 0; i < work.length; i++) {
+        char letter = work[i];
+        Set<Integer> positions = letterMap.getOrDefault(letter, Collections.emptySet());
+        if (positions.contains(i)) {
           correct++;
-        } else if (postion >= 0) {
-          close++;
+          positions.remove(i);
+          work[i] = 0;
         }
+
       }
+      for (char letter : work) {
+        if (letter != 0) {
+          Set<Integer> positions = letterMap.getOrDefault(letter, Collections.emptySet());
+          if (positions.size() > 0) {
+            close++;
+            Iterator<Integer> iter = positions.iterator();
+            iter.next();
+            iter.remove();
+          }
+        }
+
+      }
+
+
       this.correct = correct;
       this.close = close;
+    }
+
+    private Map<Character, Set<Integer>> getLetterMap(String text) {
+      Map<Character, Set<Integer>> letterMap = new HashMap<>();
+      char[] letters = text.toCharArray();
+      for (int i = 0; i < letters.length; i++) {
+          char letter = letters[i];
+          Set<Integer> positions = letterMap.getOrDefault(letter, new HashSet<>());
+          positions.add(i);
+          letterMap.putIfAbsent(letter, positions);
+
+      }
+      return letterMap;
     }
 
     @Override
